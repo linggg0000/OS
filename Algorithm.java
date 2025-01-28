b08789387ba8621d10a3babbe78d14ea4302acb1
szethin <130160059+szethin@users.noreply.github.com>
2025-01-29 00:25:05 +0800

    Update Algorithm.java
    added printGanttChart()

--- Algorithm.java
+++ Algorithm.java
@@ -8,19 +8,38 @@ abstract public class Algorithm {
     
     // Constructor
     public Algorithm (List<Process> processList) {
-        // Sort unsorted processes & store as processList
         this.processList = processList;
         this.readyQueue = new LinkedList<>();
-        //this.ganttChart = new StringBuilder();
         this.completedProcesses = new ArrayList<>();
         this.timeLine = new StringBuilder();
     }
     
-    
     // Abstract method to be overriden by algorithm classes
     abstract public void runAlgorithm();
     
-    //public void printGanttChart()
+    public void printGanttChart(int lastEndTime) {
+        StringBuilder ganttTop = new StringBuilder();
+        StringBuilder ganttMiddle = new StringBuilder();
+        StringBuilder ganttBottom = new StringBuilder();
+        
+        for (String process : completedProcesses) {
+            ganttTop.append("+--------");
+            ganttMiddle.append("|   ").append(process).append("   ");
+            ganttBottom.append("+--------");
+        }
+        
+        // Closing lines (right-most edge)
+        ganttTop.append("+");
+        ganttMiddle.append("|");
+        ganttBottom.append("+");
+        
+        System.out.println("Gantt Chart:");
+        System.out.println(ganttTop);
+        System.out.println(ganttMiddle);
+        System.out.println(ganttBottom);
+        System.out.println(timeLine.append(lastEndTime));   // Print timeline
+        System.out.println("\n\n");
+    }
         
     
 }
